e2f109ca2b5d19d48a6a7626c5af2c0c9fa3c2aa
Toby Dimmick <tobydimmick@protonmail.ch>
2015-10-30 00:16:02 +0000

    Started work on running as a gameserver w/ JSON communication.

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-nd = require('ndarray')
-underscore = require('underscore')
+const nd = require('ndarray')
+const underscore = require('underscore')
+const net = require('net')
+const jsonSocket = require('json-socket')
 
-const createNewGameGrid = (dims, mines) => {
+const server = net.createServer()
+server.listen(1066)
+server.on('connection', soc => {
+	soc = new jsonSocket(soc)
+	soc.on('message', obj => {
+		newGame(obj.dims, obj.mines)
+		soc.sendEndMessage({status: "okay"})
+	})
+})
 
-    var size = dims.reduce((x, y) => x * y)
-    if (size < mines) ; /* error? */
-    var mines_rem = mines
+const newGame = (dims, mines) => {
+	var size = dims.reduce((x, y) => x * y)
+	if (size < mines) ; /* error? */
+	var mines_rem = mines;
 
-    var gameArray = new Array(size).fill(false).fill(true, 0, mines)
-    gameArray = underscore.shuffle(gameArray)
+	var gameArray = new Array(size).fill(false).fill(true, 0, mines)
+	gameArray = underscore.shuffle(gameArray)
 
-    var gameGrid = new nd(gameArray, dims)
+	var gameGrid = new nd(gameArray, dims)
 
-    return gameGrid
+	return gameGrid
 }
 
-// console.log(createNewGameGrid([10, 3], 8))
+const client = {
+	const newGame = () => {
+		const conn = new jsonSocket(new net.Socket())
+		conn.connect(1066)
+		conn.on('connect', () => {
+			conn.sendMessage({dims: [10, 10], mines: 30})
+			conn.on('message', obj => {
+				console.log("Status: " + obj.status)
+			})
+		})
+	}
+}
+
+server.listen(1066)
+
+module.exports = {newGame: client.newGame}
