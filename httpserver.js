7529f2d37fbee66c1013b9d96ffc0ed02e443c1e
Toby Dimmick <tobydimmick@protonmail.ch>
2016-01-20 00:40:55 +0000

    Swapped real 2d array for ndarray; much faster.
    Data is now just a grid of enums.
    
    Added some error handling.

--- httpserver.js
+++ httpserver.js
@@ -1,23 +1,43 @@
+"use strict";
 const http = require('http');
 const underscore = require('underscore');
-const twoD = require('2d-array');
+const nd = require('ndarray');
 
 http.createServer((req, resp) => {
-	var body = "";
+	let body = "";
 	req.on('data', function (chunk) {
 		body += chunk;
 	});
 	req.on('end', () => {
-		resp.end(JSON.stringify(handleRequest(JSON.parse(body))));
+		let responseObj;
+		try {
+			responseObj = handleRequest(JSON.parse(body));
+		} catch(e) {
+			let errMsg;
+			if(e instanceof SyntaxError)
+				errMsg = "malformed_request";
+			else if (e.errType === "MineCount")
+				errMsg = "more mines than cells!";
+			else
+			{
+				console.log("Unhandled error: " + e.stack);
+				errMsg = "unknown_error";
+			}
+
+			responseObj = { error: errMsg };
+		}
+
+		resp.end(JSON.stringify(responseObj));
 	});
 }).listen(1066);
 
-var games = [];
+let games = {};
 
 const handleRequest = req => {
 	switch(req.action) {
 		case "newGame":
-			do /* Avoid overwriting exising game */
+			let game;
+			do /* Avoid overwriting existing game */
 				game = new Game(req.dims, req.mines);
 			while(games[game.gameState().id])
 
@@ -27,74 +47,90 @@ const handleRequest = req => {
 			break;
 
 		case "clear":
-			return games[req.id].clearCell(dims);
+			return games[req.id].clearCell(req.dims);
 			break;
 
 		case "endGame":
 			return games[req.id].reveal();
 
-		return {};
+		return { error: "no action specified" };
 	}
 }
 
 const Game = function(dims, mines) {
-	const size = dims[0]*dims[1];
+	const cellState = {
+		EMPTY: 0,
+		MINE: 1,
+		CLEARED: 2
+	}
 
-	if (mines > size){
-		/* throw error */
+	const size = dims[0]*dims[1];
+	if (mines > size) {
+		throw { errType: "MineCount" };
 	}
 
 	const id = Math.random().toString(36).substr(2,10);
-	var gameOver = false;
-	var win = false;
-	var cellsRem = size - mines;
+	let gameOver = false;
+	let win = false;
+	let cellsRem = size - mines;
 
-	var gameGrid = twoD(underscore.shuffle(
-		new Array(size).fill({}).fill({ mine: true }, 0, mines)
-	), dims[1]);
+	const gameGrid = nd(underscore.shuffle(
+			new Array(size)
+					.fill(cellState.MINE, 0, mines)
+					.fill(cellState.EMPTY, mines, size)
+	), dims);
 
-	this.gameState = (cell) => {
+	if(size < 20)
+		console.log(gameGrid);
+
+	this.gameState = cell => {
 		return {
 			id: id,
-			dims: dims,
-			mines: mines,
 			gameOver: gameOver,
 			win: win,
+			dims: dims,
+			mines: mines,
 			cellsRem: cellsRem,
 			cell: cell
 		}
 	}
 
-	this.clearCell = (dims) => {
-		var cell = this.gameGrid[dims[0]][dims[1]]
+	this.clearCell = dims => {
+		/* Representation of a cell in the grid. gets/sets gameGrid state. */
+		let cell = {
+			set: val => { gameGrid.set(dims[0], dims[1], val); },
+			state: () => { return gameGrid.get(dims[0], dims[1]); }
+		};
+
+		cell.surrounding = 0;
+		for (let i of [-1, 0, 1])
+			for (let j of [-1, 0, 1])
+				if(		(i !== 0 || j !== 0) &&
+						gameGrid.get([dims[0]+i],[dims[1]+j]) === cellState.MINE)
+					cell.surrounding++;
 
 		if(!gameOver)
 		{
-			if(!cell.surrounding)
-			{
-				cell.surrounding = 0;
-				for (i of [-1, 0, 1])
-					for (j of [-1, 0, 1])
-						if((i !== 0 || j !== 0) && this.gameGrid[dims[0]+i][dims[1]+j].mine)
-							cell.surrounding++;
-			}
-
-			if(cell.mine)
+			if(cell.state() === cellState.MINE)
 				gameOver = true;
 
-			else if (!cell.cleared) {
-				cell.cleared = true;
-				if(--cellsRem <= 0)
+			else if (cell.state() === cellState.EMPTY) {
+				cell.set(cellState.CLEARED);
+				if(--cellsRem <= 0) {
 					gameOver = true;
 					win = true;
+				}
 			}
 		}
 
-		return this.gameState(cell);
+		return this.gameState({
+			dims: dims,
+			surrounding: cell.surrounding
+		});
 	}
 
 	this.reveal = () => {
 		gameOver = true;
-		return this.gameState(gameGrid);
+		return this.gameState(gameGrid.data);
 	}
 }
