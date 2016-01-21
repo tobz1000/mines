19e8d8e8046234f68967ec61d6ed99c7a3dfbe16
Toby Dimmick <tobydimmick@protonmail.ch>
2016-01-21 13:22:20 +0000

    Class for Cells; parameter validation & handling.

--- httpserver.js
+++ httpserver.js
@@ -48,15 +48,22 @@ server.post('/action', (req, resp) => {
 let games = {};
 
 const handleRequest = req => {
-	let actionFn, id;
+	let actionName, gameAction, game;
 
-	/* TODO handle invalid id */
+	const getGame = (id) => {
+		let game = games[id];
+		if(!game)
+			throw new MinesError("unknown game id (" + id + ")");
+		return game;
+	};
+
+	/* TODO: These can probably inherit a base GameAction class? */
 	const actions = {
 		newGame : {
-			paramChecks : { dims : TY_DIMS, mines : ty.int.pos },
+			paramChecks : ty.obj.with({ dims : TY_DIMS, mines : ty.int.pos }),
 			func : () => {
-				let game;
-				do /* Avoid overwriting existing game */
+				let id;
+				do /* Avoid game id collision */
 					id = Math.random().toString(36).substr(2, GAME_ID_LEN);
 				while(games[id]);
 				game = new Game(id, req.dims, req.mines);
@@ -65,51 +72,67 @@ const handleRequest = req => {
 		},
 
 		clearCell : {
-			paramChecks : { id : TY_ID, dims : TY_DIMS },
+			paramChecks : ty.obj.with({ id : TY_ID, dims : TY_DIMS }),
 			func : () => {
-				id = req.id;
-				games[req.id].clearCell(req.dims);
+				game = getGame(req.id);
+				games.clearCell(req.dims);
 			}
 		},
 
-		// checkCell : {
-		// 	paramChecks : { id : TY_ID, dims : TY_DIMS },
-		// 	func : () => {
-		// 		id = req.id;
-		// 		games[req.id].checkCell(req.dims);
-		// 	}
-		// },
+		checkCell : {
+			paramChecks :ty.obj.with( { id : TY_ID, dims : TY_DIMS }),
+			func : () => {
+				game = getGame(req.id);
+				game.checkCell(req.dims);
+			}
+		},
 
 		endGame : {
-			paramChecks : { id : TY_ID },
+			paramChecks : ty.obj.with({ id : TY_ID }),
 			func : () => {
-				id = req.id;
-				games[req.id].endGame();
+				game = getGame(req.id);
+				game.endGame();
 			}
 		},
 
 		/* No action, just return current game state. */
 		gameState : {
-			paramChecks : { id : TY_ID }
+			paramChecks : { id : TY_ID },
+			func : () => {
+				game = getGame(req.id);
+			}
 		}
-	}
+	};
 
-	if(!req.action)
+	if(!(actionName = req.action))
 		throw new MinesError("no action specified",
 			{ available_actions: Object.keys(actions) });
 
-	if(!(actionFn = actions[req.action]))
+	if(!(gameAction = actions[actionName]))
 		throw new MinesError("unknown action", {
-			requested_action: req.action,
+			requested_action: actionName,
 			available_actions: Object.keys(actions)
 		});
 
-	/* TODO: param checks */
-
-	if(actionFn.func)
-		actionFn.func();
-
-	return games[id].gameState();
+	/*	Delete action at this point for a less confusing error message
+		(when comparing supplied vs required, since the required list won't
+		contain "action:string") */
+	delete(req.action);
+	try {
+		ty.Assert(gameAction.paramChecks)(req);
+	} catch(e) {
+		if(e instanceof ty.TypeAssertionError) {
+			throw new MinesError(
+				"invalid parameters supplied for action (" + actionName + ")", {
+					required_params : ty.Describe(gameAction.paramChecks),
+					supplied_params : req
+				}
+			)
+		}
+		else throw e;
+	}
+	gameAction.func();
+	return game.gameState();
 }
 
 const Game = function(id, dims, mines) {
@@ -117,7 +140,7 @@ const Game = function(id, dims, mines) {
 		EMPTY: 0,
 		MINE: 1,
 		CLEARED: 2
-	}
+	};
 
 	if(mines % 1 !== 0 || mines < 1)
 		throw new MinesError("invalid number of mines specified (" + mines +
@@ -134,7 +157,7 @@ const Game = function(id, dims, mines) {
 	let win = false;
 	let cellsRem = size - mines;
 	/* Information about the last cell cleared */
-	let userCell;
+	let lastUserCell;
 
 	const gameGrid = nd(_.shuffle(new Array(size)
 		.fill(cellState.MINE, 0, mines)
@@ -149,57 +172,75 @@ const Game = function(id, dims, mines) {
 			dims : dims,
 			mines : mines,
 			cellsRem : cellsRem,
-			lastCell : userCell,
-			/* TODO: current cellState enums may be confusing for user. Possible
-			solution is to create two grids: one for mine positions, and one for
-			cleared squares - so the mine grid would just be 0 and 1, easy to
-			understand. Additionally, the user could request the 'cleared' grid
-			without ending the game. So they could 'save' a game and come back
-			to it? */
+			lastCell : lastUserCell,
+			/*	TODO: current cellState enums may be confusing for user.
+				Possible solution is to create two grids: one for mine
+				positions, and one for cleared squares - so the mine grid would
+				just be 0 and 1, easy to understand. Additionally, the user
+				could request the 'cleared' grid without ending the game. So
+				they could 'save' a game and come back to it? Also we should
+				output something better than raw ndarray. */
 			grid : gameOver && verbose ? gameGrid : undefined
-		}
-	}
+		};
+	};
+
+	/* Representation of a cell in the grid. gets/sets gameGrid state. */
+	const Cell = function(dims) {
+		const surrounding = () => {
+			let surrCount = 0;
+			for (let i of [-1, 0, 1])
+				for (let j of [-1, 0, 1])
+					if((i !== 0 || j !== 0) && gameGrid.get(
+							[dims[0]+i],[dims[1]+j]) === cellState.MINE)
+						surrCount++;
+			return surrCount;
+		};
+		this.clear = () => {
+			gameGrid.set(dims[0], dims[1], cellState.CLEARED);
+		};
+		this.state = () => { return gameGrid.get(dims[0], dims[1]); };
+
+		/* Information the player is allowed to know */
+		this.userCell = () => {
+			let state = this.state();
+			if(!gameOver && state !== cellState.CLEARED)
+				state = undefined;
+
+			return {
+				dims : dims,
+				/* 'surrounding' not needed when the user can see everything */
+				surrounding : !gameOver ? surrounding() : undefined,
+				state : state
+			};
+		};
+	};
 
 	this.clearCell = dims => {
-		let surrounding;
-
 		if(gameOver)
 			throw new MinesError("game over!");
 
 		/* Representation of a cell in the grid. gets/sets gameGrid state. */
-		let cell = {
-			set: val => { gameGrid.set(dims[0], dims[1], val); },
-			state: () => { return gameGrid.get(dims[0], dims[1]); }
-		};
+		let cell = new Cell(dims);
 
 		if(cell.state() === cellState.MINE)
 			gameOver = true;
 
 		else if (cell.state() === cellState.EMPTY) {
-			cell.set(cellState.CLEARED);
+			cell.clear();
 			if(--cellsRem <= 0) {
 				gameOver = true;
 				win = true;
 			}
 		}
 
-		if(!gameOver) {
-			surrounding = 0;
-			for (let i of [-1, 0, 1])
-				for (let j of [-1, 0, 1])
-					if((i !== 0 || j !== 0) && gameGrid.get(
-							[dims[0]+i],[dims[1]+j]) === cellState.MINE)
-						surrounding++;
-		}
-
-		userCell = { dims: dims, surrounding: surrounding };
+		lastUserCell = cell.userCell();
 	}
 
 
 	/* TODO finish this, merge functionality with clearCell */
-	// this.checkCell = (dims) => {
-	// 	userCell = { dims: dims, }
-	// }
+	this.checkCell = dims => {
+		lastUserCell = new Cell(dims).userCell();
+	}
 
 	this.endGame = () => {
 		gameOver = true;
